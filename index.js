a866511c894dcfa883f59de986f3fa669f1d9f0f
Shailesh nayi <128486334+shaileshnayi@users.noreply.github.com>
2023-05-19 19:43:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+//TOGGEL ICON navbar
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuicon.onclick = () => {
+    menuicon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+//DARK MODE
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+// SCROLL SECTION ACTIVE LINK
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            });
+
+        };
+    });
+    //STICKY  navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scroll > 100);
+
+    //REMOVE TOGGLE ICON AND navbar WHEN CLICK navbar LINK (SCROLL)
+    menuicon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+// scroll reveal
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 1500,
+    delay: 150
+ });
+
+ ScrollReveal().reveal( '.home , .heading', {origin: 'top' });
+ ScrollReveal().reveal( '.services-container', {origin: 'bottom' });
+ ScrollReveal().reveal( '.home-content h1, .social-media', {origin: 'left' });
+ ScrollReveal().reveal( '.home-content p, .about-content , h3', {origin: 'right' });
